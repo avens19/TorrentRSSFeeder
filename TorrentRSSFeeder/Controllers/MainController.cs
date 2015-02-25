120f113d0d6540668075372ea59693c13bdbcdda
Andrew Ovens <mail@andrewovens.com>
2015-02-25 09:12:59 -0700

    Generalize

--- TorrentRSSFeeder/Controllers/MainController.cs
+++ TorrentRSSFeeder/Controllers/MainController.cs
@@ -24,111 +24,117 @@ namespace TorrentRSSFeeder.Controllers
         [Route("tpbtv")]
         public async Task<HttpResponseMessage> ThePirateBayTV()
         {
-            string url = ConfigurationManager.AppSettings["PirateBayTVURL"];
-            string xPath = ConfigurationManager.AppSettings["PirateBayXpath"]; 
-
-            HttpWebRequest request = WebRequest.CreateHttp(url);
-
-            request.Accept = "application/xml";
-
-            List<item> items = new List<item>();
-
-            using (var stream = (await request.GetResponseAsync()).GetResponseStream())
+            return await ParseWebsite("PirateBayTVURL", "PirateBayXpath", (td) =>
             {
-                if (stream == null)
-                    return null;
-
-                HtmlDocument doc = new HtmlDocument();
-                doc.Load(stream);
-                var nodes = doc.DocumentNode.SelectNodes(xPath);
-                foreach(HtmlNode td in nodes)
+                var description = td.Element("div").Element("a").InnerText;
+                var magnet = td.Element("a").GetAttributeValue("href", null);
+                var time = DateTime.UtcNow;
+                var timeString = HttpUtility.HtmlDecode(td.Element("font").InnerText);
+                timeString = timeString.Replace("Uploaded ", "");
+                timeString = timeString.Substring(0, timeString.IndexOf(",", StringComparison.CurrentCultureIgnoreCase));
+                bool found = false;
+                var index = timeString.IndexOf("mins", StringComparison.CurrentCultureIgnoreCase);
+                if (index > -1)
                 {
-                    var description = td.Element("div").Element("a").InnerText;
-                    var magnet = td.Element("a").GetAttributeValue("href", null);
-                    var time = DateTime.UtcNow;
-                    var timeString = HttpUtility.HtmlDecode(td.Element("font").InnerText);
-                    timeString = timeString.Replace("Uploaded ", "");
-                    timeString = timeString.Substring(0, timeString.IndexOf(",", StringComparison.CurrentCultureIgnoreCase));
-                    bool found = false;
-                    var index = timeString.IndexOf("mins", StringComparison.CurrentCultureIgnoreCase);
-                    if (index > -1)
-                    {
-                        var mins = int.Parse(timeString.Substring(0, index - 1));
-                        time = time.AddMinutes(mins*-1);
-                        found = true;
-                    }
-                    index = timeString.IndexOf("Today", StringComparison.CurrentCultureIgnoreCase);
-                    if (!found && index > -1)
-                    {
-                        var timeGMTString = timeString.Substring(6);
-                        var hours = int.Parse(timeGMTString.Substring(0, 2));
-                        var mins = int.Parse(timeGMTString.Substring(3));
-                        TimeZoneInfo gmtZone = TimeZoneInfo.FindSystemTimeZoneById("W. Europe Standard Time");
-                        DateTime timeGMT = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, gmtZone);
-                        timeGMT = timeGMT.AddHours(timeGMT.Hour*-1);
-                        timeGMT = timeGMT.AddMinutes(timeGMT.Minute*-1);
-                        timeGMT = timeGMT.AddSeconds(timeGMT.Second*-1);
-                        timeGMT = timeGMT.AddMilliseconds(timeGMT.Millisecond * -1);
-                        timeGMT = timeGMT.AddHours(hours);
-                        timeGMT = timeGMT.AddMinutes(mins);
-                        time = TimeZoneInfo.ConvertTimeToUtc(timeGMT, gmtZone);
-                        found = true;
-                    }
-                    index = timeString.IndexOf("Y-day", StringComparison.CurrentCultureIgnoreCase);
-                    if (!found && index > -1)
-                    {
-                        var timeGMTString = timeString.Substring(6);
-                        var hours = int.Parse(timeGMTString.Substring(0, 2));
-                        var mins = int.Parse(timeGMTString.Substring(3));
-                        TimeZoneInfo gmtZone = TimeZoneInfo.FindSystemTimeZoneById("W. Europe Standard Time");
-                        DateTime timeGMT = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, gmtZone);
-                        timeGMT = timeGMT.AddDays(-1);
-                        timeGMT = timeGMT.AddHours(timeGMT.Hour * -1);
-                        timeGMT = timeGMT.AddMinutes(timeGMT.Minute * -1);
-                        timeGMT = timeGMT.AddSeconds(timeGMT.Second * -1);
-                        timeGMT = timeGMT.AddMilliseconds(timeGMT.Millisecond * -1);
-                        timeGMT = timeGMT.AddHours(hours);
-                        timeGMT = timeGMT.AddMinutes(mins);
-                        time = TimeZoneInfo.ConvertTimeToUtc(timeGMT, gmtZone);
-                        found = true;
-                    }
-                    if (!found)
-                    {
-                        TimeZoneInfo gmtZone = TimeZoneInfo.FindSystemTimeZoneById("W. Europe Standard Time");
-                        DateTime timeGMT = DateTime.Parse(DateTime.UtcNow.Year + "-" + timeString);
-                        time = TimeZoneInfo.ConvertTimeToUtc(timeGMT, gmtZone);
-                    }
-                    items.Add(new item
-                    {
-                        description = description,
-                        link = magnet,
-                        pubDate = time.ToString("r"),
-                        title = description
-                    });
+                    var mins = int.Parse(timeString.Substring(0, index - 1));
+                    time = time.AddMinutes(mins * -1);
+                    found = true;
                 }
-            }
-
-            return new HttpResponseMessage
-            {
-                Content = new StringContent(new rss
+                index = timeString.IndexOf("Today", StringComparison.CurrentCultureIgnoreCase);
+                if (!found && index > -1)
                 {
-                    channel = new channel
-                    {
-                        items = items.ToArray()
-                    }
-                }.SerializeObject<rss>(),
-                Encoding.UTF8,
-                "application/rss+xml"),
-                StatusCode = HttpStatusCode.OK
-            };
+                    var timeGMTString = timeString.Substring(6);
+                    var hours = int.Parse(timeGMTString.Substring(0, 2));
+                    var mins = int.Parse(timeGMTString.Substring(3));
+                    TimeZoneInfo gmtZone = TimeZoneInfo.FindSystemTimeZoneById("W. Europe Standard Time");
+                    DateTime timeGMT = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, gmtZone);
+                    timeGMT = timeGMT.AddHours(timeGMT.Hour * -1);
+                    timeGMT = timeGMT.AddMinutes(timeGMT.Minute * -1);
+                    timeGMT = timeGMT.AddSeconds(timeGMT.Second * -1);
+                    timeGMT = timeGMT.AddMilliseconds(timeGMT.Millisecond * -1);
+                    timeGMT = timeGMT.AddHours(hours);
+                    timeGMT = timeGMT.AddMinutes(mins);
+                    time = TimeZoneInfo.ConvertTimeToUtc(timeGMT, gmtZone);
+                    found = true;
+                }
+                index = timeString.IndexOf("Y-day", StringComparison.CurrentCultureIgnoreCase);
+                if (!found && index > -1)
+                {
+                    var timeGMTString = timeString.Substring(6);
+                    var hours = int.Parse(timeGMTString.Substring(0, 2));
+                    var mins = int.Parse(timeGMTString.Substring(3));
+                    TimeZoneInfo gmtZone = TimeZoneInfo.FindSystemTimeZoneById("W. Europe Standard Time");
+                    DateTime timeGMT = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, gmtZone);
+                    timeGMT = timeGMT.AddDays(-1);
+                    timeGMT = timeGMT.AddHours(timeGMT.Hour * -1);
+                    timeGMT = timeGMT.AddMinutes(timeGMT.Minute * -1);
+                    timeGMT = timeGMT.AddSeconds(timeGMT.Second * -1);
+                    timeGMT = timeGMT.AddMilliseconds(timeGMT.Millisecond * -1);
+                    timeGMT = timeGMT.AddHours(hours);
+                    timeGMT = timeGMT.AddMinutes(mins);
+                    time = TimeZoneInfo.ConvertTimeToUtc(timeGMT, gmtZone);
+                    found = true;
+                }
+                if (!found)
+                {
+                    TimeZoneInfo gmtZone = TimeZoneInfo.FindSystemTimeZoneById("W. Europe Standard Time");
+                    DateTime timeGMT = DateTime.Parse(DateTime.UtcNow.Year + "-" + timeString);
+                    time = TimeZoneInfo.ConvertTimeToUtc(timeGMT, gmtZone);
+                }
+                return new item
+                {
+                    description = description,
+                    link = magnet,
+                    pubDate = time.ToString("r"),
+                    title = description
+                };
+            });
         }
 
         [HttpGet]
         [Route("eztv")]
         public async Task<HttpResponseMessage> EZTV()
         {
-            string url = ConfigurationManager.AppSettings["EZTVURL"];
-            string xPath = ConfigurationManager.AppSettings["EZTVXpath"];
+            return await ParseWebsite("EZTVURL", "EZTVXpath", (tr) =>
+            {
+                var description = tr.Elements("td").ToArray()[1].Element("a").InnerText;
+                var magnet = tr.Elements("td").ToArray()[2].Element("a").GetAttributeValue("href", null);
+                var time = DateTime.UtcNow;
+                var timeString = tr.Elements("td").ToArray()[3].InnerText.Trim();
+                var timeStrings = timeString.Split(' ');
+                foreach (var ts in timeStrings)
+                {
+                    if (ts.IndexOf("d", StringComparison.CurrentCultureIgnoreCase) > -1)
+                    {
+                        time = time.AddDays(int.Parse(ts.Substring(0, ts.Length - 1)) * -1);
+                    }
+                    else if (ts.IndexOf("h", StringComparison.CurrentCultureIgnoreCase) > -1)
+                    {
+                        time = time.AddHours(int.Parse(ts.Substring(0, ts.Length - 1)) * -1);
+                    }
+                    else if (ts.IndexOf("m", StringComparison.CurrentCultureIgnoreCase) > -1)
+                    {
+                        time = time.AddMinutes(int.Parse(ts.Substring(0, ts.Length - 1)) * -1);
+                    }
+                    else if (ts.IndexOf("s", StringComparison.CurrentCultureIgnoreCase) > -1)
+                    {
+                        time = time.AddSeconds(int.Parse(ts.Substring(0, ts.Length - 1)) * -1);
+                    }
+                }
+                return new item
+                {
+                    description = description,
+                    link = magnet,
+                    pubDate = time.ToString("r"),
+                    title = description
+                };
+            });
+        }
+        
+        private async Task<HttpResponseMessage> ParseWebsite(string urlSetting, string xPathSetting, Func<HtmlNode, item> handleItem)
+        {
+            string url = ConfigurationManager.AppSettings[urlSetting];
+            string xPath = ConfigurationManager.AppSettings[xPathSetting];
 
             HttpWebRequest request = WebRequest.CreateHttp(url);
 
@@ -144,39 +150,9 @@ namespace TorrentRSSFeeder.Controllers
                 HtmlDocument doc = new HtmlDocument();
                 doc.Load(stream);
                 var nodes = doc.DocumentNode.SelectNodes(xPath);
-                foreach (HtmlNode tr in nodes)
+                foreach (HtmlNode node in nodes)
                 {
-                    var description = tr.Elements("td").ToArray()[1].Element("a").InnerText;
-                    var magnet = tr.Elements("td").ToArray()[2].Element("a").GetAttributeValue("href", null);
-                    var time = DateTime.UtcNow;
-                    var timeString = tr.Elements("td").ToArray()[3].InnerText.Trim();
-                    var timeStrings = timeString.Split(' ');
-                    foreach (var ts in timeStrings)
-                    {
-                        if (ts.IndexOf("d", StringComparison.CurrentCultureIgnoreCase) > -1)
-                        {
-                            time = time.AddDays(int.Parse(ts.Substring(0, ts.Length - 1))*-1);
-                        }
-                        else if (ts.IndexOf("h", StringComparison.CurrentCultureIgnoreCase) > -1)
-                        {
-                            time = time.AddHours(int.Parse(ts.Substring(0, ts.Length - 1)) * -1);
-                        }
-                        else if (ts.IndexOf("m", StringComparison.CurrentCultureIgnoreCase) > -1)
-                        {
-                            time = time.AddMinutes(int.Parse(ts.Substring(0, ts.Length - 1)) * -1);
-                        }
-                        else if (ts.IndexOf("s", StringComparison.CurrentCultureIgnoreCase) > -1)
-                        {
-                            time = time.AddSeconds(int.Parse(ts.Substring(0, ts.Length - 1)) * -1);
-                        }
-                    }
-                    items.Add(new item
-                    {
-                        description = description,
-                        link = magnet,
-                        pubDate = time.ToString("r"),
-                        title = description
-                    });
+                    items.Add(handleItem(node));
                 }
             }
 
